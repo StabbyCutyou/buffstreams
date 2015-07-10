6504f7b710b480f7f9e8c79a101a4cee96353848
StabbyCutyou <smattkelly@gmail.com>
2015-07-10 13:39:09 -0400

    Scratch work in test/test.go

--- test/test.go
+++ test/test.go
@@ -5,6 +5,7 @@ import (
 	"github.com/StabbyCutyou/buffstreams/test/message"
 	"github.com/golang/protobuf/proto"
 	"log"
+	"net"
 	"strconv"
 	"time"
 )
@@ -38,16 +39,78 @@ func main() {
 			address := buffstreams.FormatAddress("127.0.0.1", port)
 			count := 0
 			for {
-				written, err := bm.WriteTo(address, msgBytes, true)
-				log.Printf("Wrote %d Bytes of %d + 2, error was: %s", written, len(msgBytes), err)
-				//if err != nil {
-				//	log.Printf("Error %s", err)
-				//}
+				_, err := bm.WriteTo(address, msgBytes, true)
+				//log.Printf("Wrote %d Bytes of %d + 2, error was: %s", written, len(msgBytes), err)
+				if err != nil {
+					log.Printf("Error %s", err)
+				}
 				count = count + 1
 				log.Printf("%d - %d", n, count)
 			}
 		}(i, strconv.Itoa(startingPort), bm)
 		//startingPort = startingPort + 1
 	}
+	time.Sleep(time.Minute * 10)
+}
+
+func testTCPConns() {
+	// Start Listening
+	go func() {
+		//readTicker := time.NewTicker(10 * time.Millisecond)
+		tcpAddr, err := net.ResolveTCPAddr("tcp", ":5031")
+		if err != nil {
+			log.Print(err)
+			return
+		}
+		receiveSocket, err := net.ListenTCP("tcp", tcpAddr)
+		if err != nil {
+			log.Print(err)
+			return
+		}
+
+		conn, err := receiveSocket.AcceptTCP()
+		if err != nil {
+			log.Printf("Error attempting to accept connection: %s", err)
+		} else {
+			buffer := make([]byte, 10)
+			for {
+				//select {
+				// Check to see if we have a tick
+				//case <-readTicker.C:
+				log.Print("Trying to read")
+				bytesRead, err := conn.Read(buffer)
+				log.Printf("Bytes Read: %d out of %d", bytesRead, 10)
+				if err != nil {
+					log.Printf("Error: %s", err)
+				}
+				//}
+			}
+		}
+	}()
+
+	// Start writing
+	go func() {
+		writeTicker := time.NewTicker(10 * time.Second)
+		tcpAddr, err := net.ResolveTCPAddr("tcp", "127.0.0.1:5031")
+		if err != nil {
+			log.Print(err)
+			return
+		}
+		bytes := []byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+		conn, err := net.DialTCP("tcp", nil, tcpAddr)
+		for {
+			select {
+			// Check to see if we have a tick
+			case <-writeTicker.C:
+				log.Printf("%s", time.Now())
+				bytesWritten, err := conn.Write(bytes)
+				log.Printf("Bytes Written: %d out of %d", bytesWritten, 10)
+				if err != nil {
+					log.Printf("Error: %s", err)
+				}
+			}
+		}
+	}()
+
 	time.Sleep(time.Minute * 10)
 }
