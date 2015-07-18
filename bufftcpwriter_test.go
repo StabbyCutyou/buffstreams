dbedc28222b5807ceb8fc5991683b261f226128d
StabbyCutyou <smattkelly@gmail.com>
2015-07-17 21:51:44 -0400

    Writer benchmark - assumes you have the test server running
    writer test no longer needs external server, but is now a little slower

--- bufftcpwriter_test.go
+++ bufftcpwriter_test.go
@@ -1,6 +1,46 @@
 package buffstreams
 
-import "testing"
+import (
+	"strconv"
+	"testing"
+	"time"
+
+	"github.com/StabbyCutyou/buffstreams/test/message"
+	"github.com/golang/protobuf/proto"
+)
+
+func exampleCallback(bts []byte) error {
+	msg := &message.Note{}
+	err := proto.Unmarshal(bts, msg)
+	return err
+}
+
+var (
+	writeConfig = BuffTCPWriterConfig{
+		MaxMessageSize: 2048,
+		EnableLogging:  true,
+		Address:        FormatAddress("127.0.0.1", strconv.Itoa(5031)),
+	}
+
+	listenConfig = BuffTCPListenerConfig{
+		MaxMessageSize: 2048,
+		EnableLogging:  true,
+		Address:        FormatAddress("", strconv.Itoa(5031)),
+		Callback:       exampleCallback,
+	}
+
+	btl = func() *BuffTCPListener {
+		buffL := NewBuffTCPListener(listenConfig)
+		buffL.StartListeningAsync()
+		return buffL
+	}()
+	btw      = func() *BuffTCPWriter { buffT := NewBuffTCPWriter(writeConfig); buffT.Open(); return buffT }()
+	name     = "Stabby"
+	date     = time.Now().UnixNano()
+	data     = "This is an intenntionally long and rambling sentence to pad out the size of the message."
+	msg      = &message.Note{Name: &name, Date: &date, Comment: &data}
+	msgBytes = func(*message.Note) []byte { b, _ := proto.Marshal(msg); return b }(msg)
+)
 
 func TestBuffTCPWriterUsesDefaultMessageSize(t *testing.T) {
 	cfg := BuffTCPWriterConfig{}
@@ -17,5 +57,11 @@ func TestBuffTCPWriterUsesSpecifiedMaxMessageSize(t *testing.T) {
 	buffM := NewBuffTCPWriter(cfg)
 	if buffM.maxMessageSize != cfg.MaxMessageSize {
 		t.Errorf("Expected Max Message Size to be %d, actually got %d", cfg.MaxMessageSize, buffM.maxMessageSize)
+	}
+}
+
+func BenchmarkWrite(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		btw.Write(msgBytes)
 	}
 }
